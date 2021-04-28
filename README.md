237c4cbd9abac9bab9b927f302826d76c6b88ca3
Ferdinand Polpol <ferdinand@countable.ca>
2021-04-28 10:38:48 +0800

    updates

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Host bootstrap and jobs, for OpenOSP
 
 ## Setup
+You should be the root user when setting up the open osp server
 ```
 git clone https://github.com/open-osp/open-osp-server
 cd open-osp-server
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+set -ex
+
 cd $HOME
 
 git clone https://github.com/countable-web/dotfiles.git
 
 cd $HOME/dotfiles/deploy
 
-./setup-jenkins-slave.sh
+exists=$(grep -c "^jenkins:" /etc/passwd)
+
+if [ $exists -eq 0 ]; then
+	./setup-jenkins-slave.sh
+else
+    echo "The user jenkins already exists"
+fi
+
 ./setup-docker.sh
 
 fallocate -l 10G /10gb
@@ -19,7 +28,7 @@ apt install cron
 
 echo "00 10 * * * jenkins cd /home/jenkins/open-osp-server && ./bin/updater.sh backup >> /home/jenkins/backups.log 2>&1" >> /etc/crontab
 echo "00 * * * * jenkins cd /home/jenkins/open-osp-server && ./bin/updater.sh check >> /home/jenkins/checks.log 2>&1" >> /etc/crontab
-echo "00 00 * * * jenkins cd /home/jenkins/open-osp-server && ./bin/updater.sh usage >> /home/jenkins/checks.log 2>&1" >> /etc/crontab
+echo "00 00 * * * admin cd /home/jenkins/open-osp-server && ./bin/updater.sh usage >> /home/jenkins/checks.log 2>&1" >> /etc/crontab
 
 
 cd $HOME
