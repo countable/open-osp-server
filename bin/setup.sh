97e1505e70532f9e513510b49ee0991e01390d1a
Your Name <you@example.com>
2021-04-28 21:08:54 +0800

    remove notes in oscar rest

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,37 +4,37 @@ set -uxo
 
 cd $HOME
 
-if [ -z $HOME/dotfiles ]; then
-	git clone https://github.com/countable-web/dotfiles.git
-else
-	echo "Skipping, dotfiles already exists"
-fi
+# if [ -z $HOME/dotfiles ]; then
+# 	git clone https://github.com/countable-web/dotfiles.git
+# else
+# 	echo "Skipping, dotfiles already exists"
+# fi
 
-exists=$(grep -c "^jenkins:" /etc/passwd)
+# exists=$(grep -c "^jenkins:" /etc/passwd)
 
-if [ $exists -eq 0 ]; then
-	$HOME/dotfiles/deploy/setup-jenkins-slave
-else
-    echo "The user jenkins already exists"
-fi
+# if [ $exists -eq 0 ]; then
+# 	$HOME/dotfiles/deploy/setup-jenkins-slave
+# else
+#     echo "The user jenkins already exists"
+# fi
 
-bash $HOME/dotfiles/deploy/unpack &
-bash $HOME/dotfiles/deploy/setup-docker &
+# bash $HOME/dotfiles/deploy/unpack &
+# bash $HOME/dotfiles/deploy/setup-docker &
 
-echo "Sleep 30 seconds, waiting for unpack and docker"
-sleep 30
+# echo "Sleep 30 seconds, waiting for unpack and docker"
+# sleep 30
 
-fallocate -l 10G /10gb
+# fallocate -l 10G /10gb
 
-$HOME/dotfiles/bin/make-swapfile ${1:-60G}
+# $HOME/dotfiles/bin/make-swapfile ${1:-60G}
 
-apt install cron
+# apt install cron
 
-echo "00 10 * * * jenkins cd /home/jenkins/open-osp-server && ./bin/updater.sh backup >> /home/jenkins/backups.log 2>&1" >> /etc/crontab
-echo "00 * * * * jenkins cd /home/jenkins/open-osp-server && ./bin/updater.sh check >> /home/jenkins/checks.log 2>&1" >> /etc/crontab
-echo "00 00 * * * admin cd /home/jenkins/open-osp-server && ./bin/updater.sh usage >> /home/jenkins/checks.log 2>&1" >> /etc/crontab
+# echo "00 10 * * * jenkins cd /home/jenkins/open-osp-server && ./bin/updater.sh backup >> /home/jenkins/backups.log 2>&1" >> /etc/crontab
+# echo "00 * * * * jenkins cd /home/jenkins/open-osp-server && ./bin/updater.sh check >> /home/jenkins/checks.log 2>&1" >> /etc/crontab
+# echo "00 00 * * * admin cd /home/jenkins/open-osp-server && ./bin/updater.sh usage >> /home/jenkins/checks.log 2>&1" >> /etc/crontab
 
 
-sudo -u jenkins -H sh -c "./haproxy.sh"
+sudo -u jenkins -H sh -c "`dirname "$0"`/haproxy.sh"
 
 exit 0
