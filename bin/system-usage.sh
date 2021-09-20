d17f9c2eb79d312288e4fad8d26d61c12c651804
Your Name <you@example.com>
2021-09-20 05:59:08 +0000

    exclude overlay

--- bin/system-usage.sh
+++ bin/system-usage.sh
@@ -6,6 +6,7 @@ message=$(df -h | awk -v ALERT="$ALERT" '
     NR == 1 {next}
     $1 == "abc:/xyz/pqr" {next}
     $1 == "tmpfs" {next}
+    $1 == "overlay" {next}
     $1 == "/dev/cdrom" {next}
     1 {sub(/%/,"",$5)}
     $5 >= ALERT {printf "%s is almost full: %d%%\n", $1, $5}
