6150a036530218c6bd5aa287e68fa92089e28a5b
Your Name <you@example.com>
2022-03-24 05:36:19 +0000

    fix notifications

--- bin/sendgrid.sh
+++ bin/sendgrid.sh
@@ -16,8 +16,15 @@ echo $bodyHTML
 maildata='{"personalizations": [{"to": [{"email": "'${EMAIL_TO}'"}]}],"from": {"email": "'${FROM_EMAIL}'", 
 	"name": "'${FROM_NAME}'"},"subject": "'${SUBJECT}'","content": [{"type": "text/html", "value": "'${bodyHTML}'"}]}'
 
+#curl --request POST \
+#  --url https://api.sendgrid.com/v3/mail/send \
+#  --header 'Authorization: Bearer '$SENDGRID_API_KEY \
+#  --header 'Content-Type: application/json' \
+#  --data "'$maildata'"
+
+echo $bodyHTML
 curl --request POST \
   --url https://api.sendgrid.com/v3/mail/send \
-  --header 'Authorization: Bearer '$SENDGRID_API_KEY \
+  --header "Authorization: Bearer ${SENDGRID_API_KEY}" \
   --header 'Content-Type: application/json' \
-  --data "'$maildata'"
+  --data "{\"personalizations\": [{\"to\": [{\"email\": \"clark@countable.ca\"}]}],\"from\": {\"email\": \"root@openosp.ca\"},\"subject\": \"disk usage high on $HOSTNAME\",\"content\": [{\"type\": \"text/plain\", \"value\": \"${bodyHTML}\"}]}" 
