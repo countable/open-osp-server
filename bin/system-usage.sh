d593123dd235dff3aaaf7553e3aec172bdb7d01d
Your Name <you@example.com>
2022-11-11 18:58:10 -0800

    memory monitor

--- bin/system-usage.sh
+++ bin/system-usage.sh
@@ -2,7 +2,7 @@
 
 ALERT=90
 
-message=$(df -h | awk -v ALERT="$ALERT" '
+diskmessage=$(df -h | awk -v ALERT="$ALERT" '
     NR == 1 {next}
     $1 == "abc:/xyz/pqr" {next}
     $1 == "tmpfs" {next}
@@ -11,6 +11,15 @@ message=$(df -h | awk -v ALERT="$ALERT" '
     1 {sub(/%/,"",$5)}
     $5 >= ALERT {printf "%s is almost full: %d%%\n", $1, $5}
 ')
+
+memorymessage=""
+musage=$(free | awk '/Mem/{printf("RAM Usage: %.2f%\n"), $3/$2*100}' |  awk '{print $3}' | cut -d"." -f1)
+if [ $musage -ge $ALERT ]; then
+   memorymessage="Current Memory Usage: $musage%\n"
+fi
+
+message="$diskmessage$memorymessage"
+
 if [ -n "$message" ]; then
   echo "$message"
   htmlMessage="${message//$'\n'/<br />}"
