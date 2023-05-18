410c19c73d539b59c344717ce65d56b23b8fd5f1
Your Name <you@example.com>
2023-05-18 14:14:52 +0000

    handle 100% full case

--- bin/system-usage.sh
+++ bin/system-usage.sh
@@ -2,13 +2,14 @@
 
 ALERT=90
 
-message=$(df -h | awk -v ALERT="$ALERT" '
+message=$(df -P | awk -v ALERT="$ALERT" '
     NR == 1 {next}
     $1 == "abc:/xyz/pqr" {next}
     $1 == "tmpfs" {next}
     $1 == "overlay" {next}
     $1 == "/dev/cdrom" {next}
-    1 {sub(/%/,"",$5)}
+    $1 ~ /^\/dev\/loop/ {next}
+    1 {sub(/%/,"",$5); $5 += 0}
     $5 >= ALERT {printf "%s is almost full: %d%%\n", $1, $5}
 ')
 if [ -n "$message" ]; then
